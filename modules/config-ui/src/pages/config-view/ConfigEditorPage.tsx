af1850b61d8c43862b1638e31593f85f4c8fa0d9
Arun Rajput <arunrajput@Aruns-MacBook-Pro-2.local>
2022-05-23 10:34:01 +0530

    fix: modules and sections using schema json

--- modules/config-ui/src/pages/config-view/ConfigEditorPage.tsx
+++ modules/config-ui/src/pages/config-view/ConfigEditorPage.tsx
@@ -4,6 +4,8 @@ import {
   Box,
   Heading,
   Icon,
+  Link,
+  Switch,
   Tab,
   TabList,
   TabPanel,
@@ -13,38 +15,131 @@ import {
 
 import { FaInfoCircle } from "react-icons/fa";
 
-const ConfigEditorPage = () => {
-  const { t } = useTranslation("configui");
+import Form from "@rjsf/chakra-ui";
+import attendanceConfigSchema from "../../services/Attendance/attendance-config-schema.json";
+import { useState } from "react";
 
+const ObjectFieldTemplate = (props: any) => {
+  return (
+    <div>
+      <Heading
+        m={1}
+        display={"flex"}
+        as="h4"
+        size="sm"
+        backgroundColor="primary.900"
+      >
+        <Switch m={2} id="enable-m1" />
+        <Box m={2} flex={1}>
+          {props.title}
+        </Box>
+      </Heading>
+      <Box>
+        {props.properties.map((element: any, index: number) => (
+          <div key={index} className="property-wrapper">
+            {element.content}
+          </div>
+        ))}
+      </Box>
+    </div>
+  );
+};
+
+const CustomFieldTemplate = (props: any) => {
+  const {
+    id,
+    classNames,
+    label,
+    help,
+    required,
+    description,
+    errors,
+    children,
+  } = props;
+  return (
+    <div className={classNames}>
+      {description}
+      {children}
+      {errors}
+      {help}
+    </div>
+  );
+};
+
+interface ConfigSchema {
+  name: string;
+  label: string;
+  subModules: SubModuleConfig[];
+}
+interface SubModuleConfig {
+  name: string;
+  label: string;
+  sections: SectionConfig[];
+}
+interface SectionConfig {
+  name: string;
+  label: string;
+  schema?: any;
+  uischema?: any;
+}
+
+const ConfigEditorPage = () => {
+  const config: any = attendanceConfigSchema;
+
+  const [selectedModule, setSelectedModule] = useState<SubModuleConfig>(
+    config["subModules"][0]
+  );
+  const { t } = useTranslation("configui");
   return (
     <Box marginX={4}>
       <Heading as="h4" size="md">
-        Attendance <Icon as={FaInfoCircle} />
+        {config.label} <Icon as={FaInfoCircle} />
       </Heading>
       <Box marginY={4} p={0}>
-        <Button
-          style={{ borderRadius: 0, margin: "0px" }}
-          color="primary.100"
-          variant="outline"
-        >
-          Student Attendance
-        </Button>
-        <Button style={{ borderRadius: 0, margin: "0px" }} variant="outline">
-          Staff Attendance
-        </Button>
+        {config["subModules"].map((m: SubModuleConfig, index: number) => {
+          return (
+            <Button
+              key={index}
+              style={{ borderRadius: 0, margin: "0px" }}
+              color="primary.100"
+              variant="outline"
+            >
+              <Link
+                onClick={() => {
+                  setSelectedModule(m);
+                }}
+              >
+                {m.label}
+              </Link>
+            </Button>
+          );
+        })}
       </Box>
       <Box>
         <Tabs>
           <TabList>
-            <Tab>Markinng Attendance</Tab>
-            <Tab>Reports</Tab>
-            <Tab>Communication</Tab>
+            {selectedModule.sections.map(
+              (section: SectionConfig, index: number) => {
+                return <Tab key={index}>{section.label}</Tab>;
+              }
+            )}
           </TabList>
 
           <TabPanels>
-            <TabPanel>Markinng Attendance Configuration</TabPanel>
-            <TabPanel>Reports Configuration</TabPanel>
-            <TabPanel>Communication Configuration</TabPanel>
+            {selectedModule.sections.map(
+              (section: SectionConfig, index: number) => {
+                return (
+                  <TabPanel key={index}>
+                    <Form
+                      schema={section.schema}
+                      uiSchema={section.uischema}
+                      ObjectFieldTemplate={ObjectFieldTemplate}
+                      FieldTemplate={CustomFieldTemplate}
+                    />
+                  </TabPanel>
+                );
+              }
+            )}
           </TabPanels>
         </Tabs>
       </Box>
