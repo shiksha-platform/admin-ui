24b225f47b92bd2991a3c21a48d25b8627de00aa
Arun Rajput <arunrajput@Aruns-MacBook-Pro-2.local>
2022-06-15 11:53:05 +0530

    fix: add deploy  script

---
+++ scripts/pack-prod-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm -rf prod-build
+mkdir prod-build
+mkdir prod-build/modules
+find modules \( ! -path lib/common-ui -o ! -path apps/admiin-console \) -type d  -maxdepth 1 -mindepth 1 -exec bash -c '
+for f  do
+    # echo $f
+    if [ $f != "lib/common-ui" ] &&  [ $f != "apps/admin-console" ] ; then
+        echo "Processing ${f//modules\//}"
+        cp -rf "$f/build" "prod-build/modules/${f//modules\//}"
+    fi
+done 
+' sh {} +
+cp -r  apps/admin-console/build/* prod-build/
+find  prod-build -name  'modules.json' | xargs sed -i 's|http://localhost:[0-9]*||g'
+cd prod-build && tar -cf ../shiksha-admin-ui.tar . && cd ../
+          
+          
+
