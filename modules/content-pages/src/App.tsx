87e35221af5637d20b758c85b4308cd4c6c9fea9
Hardik Sharma <hardik00051@gmail.com>
2022-07-28 13:59:40 +0530

    Feat:Added content-pages module

---
+++ modules/content-pages/src/App.tsx
@@ -0,0 +1,43 @@
+import "./App.css";
+
+import "common-ui/dist/index.css";
+
+import {
+  AppShell,
+  AppShellProps,
+  initializeI18n,
+  LoginPage,
+  NavMenuItemProps,
+} from "common-ui";
+
+type User = {
+  name: string;
+};
+
+const AboutPage = () => <h1>About Page</h1>;
+
+initializeI18n([], `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`);
+
+function App() {
+  const routes = [
+    {
+      path: "about",
+      label: "About",
+      component: AboutPage,
+    },
+  ];
+  let navLinks: NavMenuItemProps[] = [];
+  const appShellProp: AppShellProps = {
+    label: "",
+    routes: routes,
+    navLinks: navLinks,
+    AuthComponent: LoginPage,
+  };
+  return (
+    <article>
+      <AppShell {...appShellProp}></AppShell>
+    </article>
+  );
+}
+
+export default App;
