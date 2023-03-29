1c15b2405c8582444066ba8de5df2aa350a6291c
snehal patil <snehalpatil0904@gmail.com>
2023-03-29 22:03:46 +0530

    Server change

--- modules/config-ui/src/services/ConfigService.ts
+++ modules/config-ui/src/services/ConfigService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import attendanceConfigSchema from "../services/Attendance/attendance-config-schema.json";
 
-const baseUrl = "https://sandbox.shikshaplatform.io/api/v1";
+const baseUrl = "https://sandbox.shiksha.samagra.io/api/v1";
 const fetchConfigApiUrl = baseUrl + "/config/{module}/all";
 const saveConfigApiUrl = baseUrl + "/config/{multipleConfigs}";
 
